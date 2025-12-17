57a15675f5781adf55d17483a64f7563b551050a
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-12-17 11:39:25 +0100

    now everything that needs to be owned in the db is actually owned

--- src/domain/TechSkill.ts
+++ src/domain/TechSkill.ts
@@ -1,15 +1,13 @@
 export interface TechSkillRow {
   id: number;
+  owner_id: string;
   name: string;
 }
 
 export class TechSkill {
-  constructor(
-    public id: number,
-    public name: string
-  ) {}
+  constructor(public id: number, public ownerId: string, public name: string) {}
 
   static fromRow(row: TechSkillRow): TechSkill {
-    return new TechSkill(row.id, row.name);
+    return new TechSkill(row.id, row.owner_id, row.name);
   }
 }
