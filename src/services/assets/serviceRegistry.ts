304b2c66f8efd659205ad3e868b92dfaa662bd80
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-12-26 11:40:34 +0100

    updated service registry to include relationservice and whether they are positionned

--- src/services/assets/serviceRegistry.ts
+++ src/services/assets/serviceRegistry.ts
@@ -7,20 +7,67 @@ import { summaryService } from "@/services/summary/SummaryService";
 import { softSkillService } from "@/services/skills/SoftSkillService";
 import { techSkillService } from "@/services/skills/TechSkillService";
 import { languageSkillService } from "../skills/LanguageSkillService";
+import {
+  cvEducationRelations,
+  cvExperienceRelations,
+  cvLanguageRelations,
+  cvProfessionRelations,
+  cvSoftSkillRelations,
+  cvSummaryRelations,
+  cvTechSkillRelations,
+} from "../cv/CvRelationsService";
+import { CvRelationService } from "../base/CvRelationService";
 
 export interface AssetServiceEntry {
   service: CrudService<any, any, any, any>;
   translatable: boolean;
+  relationservice: CvRelationService<any, any> | null;
+  positionned: boolean;
 }
 
 export const assetServiceRegistry: Record<AssetType, AssetServiceEntry> = {
-  education: { service: educationService, translatable: true },
-  experience: { service: experienceService, translatable: true },
-  profession: { service: professionService, translatable: true },
-  summaries: { service: summaryService, translatable: true },
-  softskills: { service: softSkillService, translatable: true },
-  techskills: { service: techSkillService, translatable: false },
-  languageskills: { service: languageSkillService, translatable: true },
+  education: {
+    service: educationService,
+    translatable: true,
+    relationservice: cvEducationRelations,
+    positionned: true,
+  },
+  experience: {
+    service: experienceService,
+    translatable: true,
+    relationservice: cvExperienceRelations,
+    positionned: true,
+  },
+  summaries: {
+    service: summaryService,
+    translatable: true,
+    relationservice: cvSummaryRelations,
+    positionned: false,
+  },
+  softskills: {
+    service: softSkillService,
+    translatable: true,
+    relationservice: cvSoftSkillRelations,
+    positionned: true,
+  },
+  techskills: {
+    service: techSkillService,
+    translatable: false,
+    relationservice: cvTechSkillRelations,
+    positionned: true,
+  },
+  languageskills: {
+    service: languageSkillService,
+    translatable: true,
+    relationservice: cvLanguageRelations,
+    positionned: true,
+  },
+  profession: {
+    service: professionService,
+    translatable: false,
+    relationservice: cvProfessionRelations,
+    positionned: false,
+  },
 };
 
 export function getAssetServiceEntry(
