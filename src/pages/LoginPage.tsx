e0cd837d4c1ceb11fbfff07318371474d6dcb14e
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2026-01-13 17:44:01 +0100

    background of LoginPage done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,23 @@
 import { AuthButton } from "@/components/auth/AuthButton";
-
+import "./LoginPage.css";
 export default function LoginPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-slate-900 to-slate-800">
-      <div className="text-center">
-        <h1 className="text-7xl font-bold text-white mb-4 tracking-tight">
+    <div className="min-h-screen flex flex-col items-center justify-center ">
+      <div>
+        <h1 className="mt-20 font-anton text-7xl font-bold text-black mb-4 tracking-tight">
           CVs' Lab
         </h1>
-        <p className="text-xl text-slate-300 mb-12">
-          Create, manage, and export your professional CVs
-        </p>
-        <AuthButton />
       </div>
+      <div className="scene">
+        <div className="cv">
+          <div className="text-center flex flex-col justify-center items-center h-full p-6">
+            <p className="text-xl text-black font-anton">
+              Create, manage, and export your professional CVs
+            </p>
+          </div>
+        </div>
+      </div>
+      <AuthButton />
     </div>
   );
 }
